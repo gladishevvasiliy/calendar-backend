0b41a37de46d5e6847e92a5720772546c3867c08
Vasiliy.Gladyshev <Vasiliy.Gladyshev@x5.ru>
2021-03-11 14:39:51 +0300

    add returning json

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+// const mysql = require("mysql");
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-express()
-  .get("/", (req, res) => res.send("Hello, Joe!"))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// var connection = mysql.createConnection({
+//   host: "vh302.timeweb.ru",
+//   user: "co48677_wp",
+//   password: "vasyok97",
+//   database: "co48677_wp",
+// });
+
+// connection.connect();
+
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
