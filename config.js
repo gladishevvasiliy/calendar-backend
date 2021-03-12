38196af335c77a7b1cc665486de3f8317c7e3074
Vasiliy.Gladyshev <Vasiliy.Gladyshev@x5.ru>
2021-03-12 09:47:31 +0300

    add google config

--- config.js
+++ config.js
@@ -1,11 +1,17 @@
 const config = {
   db: {
-    host: "vh302.timeweb.ru",
-    port: "3306",
-    user: "co48677_wp",
-    password: "vasyok971",
-    database: "co48677_wp",
+    host: "34.78.225.155",
+    user: "root",
+    password: "vasyok97",
+    database: "mydbcalendar-db",
   },
+  // db: {
+  //   host: "vh302.timeweb.ru",
+  //   port: "3306",
+  //   user: "co48677_wp",
+  //   password: "vasyok97",
+  //   database: "co48677_wp",
+  // },
   listPerPage: 10,
 };
 
