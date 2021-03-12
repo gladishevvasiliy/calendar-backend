4025c126324cec1ca576a307c8679529dedf067d
Vasiliy.Gladyshev <Vasiliy.Gladyshev@x5.ru>
2021-03-12 13:54:44 +0300

    add correct config, localhost connection getted

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const config = {
-  db: {
-    host: "34.78.225.155",
-    user: "root",
-    password: "vasyok97",
-    database: "mydbcalendar-db",
-  },
   // db: {
-  //   host: "vh302.timeweb.ru",
-  //   port: "3306",
-  //   user: "co48677_wp",
+  //   host: "34.78.225.155",
+  //   user: "root",
   //   password: "vasyok97",
-  //   database: "co48677_wp",
+  //   database: "mydbcalendar-db",
   // },
+  db: {
+    host: "vh302.timeweb.ru",
+    port: "3306",
+    user: "co48677_wp",
+    password: "vasyok97",
+    database: "co48677_wp",
+  },
   listPerPage: 10,
 };
 
