cca78f005514952b353eee2b68768fa0914a1818
Vasiliy.Gladyshev <Vasiliy.Gladyshev@x5.ru>
2021-03-18 22:09:54 +0300

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const calendarRouter = require("./routes/calendar");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
